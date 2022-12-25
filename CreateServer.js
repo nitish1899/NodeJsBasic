0fbd50bc3d055d37ff5dc471511a7f57d1adc954
nitish1899 <nkword1899@gmail.com>
2022-12-26 00:59:35 +0530

    Client fetching response with different url

--- CreateServer.js
+++ CreateServer.js
@@ -1,9 +1,37 @@
+//const http = require('http');
 const http = require('http');
-
 const server = http.createServer((req,res) => {
-    console.log(req);
-    res.writeHead(200);
+    //console.log(req.url,req.method,req.headers);
+    const url= req.url;
+    if (url === '/home'){
+    res.write('<html>');
+    res.write('<head><title>My First Page </title></head>');
+    res.write('<body><h1> Welcome home</h1></body>');
+    res.write('</html>');
     res.end("Nitish Kumar");
+    } else if(url === '/about'){
+        res.write('<html>');
+    res.write('<head><title>My First Page </title></head>');
+    res.write('<body><h1> Welcome to About Us page</h1></body>');
+    res.write('</html>');
+    res.end("Nitish Kumar");
+    } else if(url === '/node'){
+        res.write('<html>');
+    res.write('<head><title>My First Page </title></head>');
+    res.write('<body><h1> Welcome to my Node Js project</h1></body>');
+    res.write('</html>');
+    res.end("Nitish Kumar");
+    }
+    else {
+        res.write('<html>');
+        res.write('<head><title>My First Page </title></head>');
+        res.write('<body><h1> Welcome Back Again </h1></body>');
+        res.write('</html>');
+        res.end("Nitish Kumar");
+    }
+    res.setHeader('Content-Type','text/html');
+    res.writeHead(200);
+    
 });
 
 server.listen(4000 , ()=> {
